94851179228fc4a4896eddd327bf4023449f70fa
bingjiew1027-Crystal <bingjiew1027@gmail.com>
2025-11-18 01:26:33 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 # Flask
+
+from flask import Flask,render template, request
+
+app = Flask(_name_)
+
+@app.route("/",methods=["GET","POST"])
+ def index():
+return(render template("index.html"))
+
+if_name_== "_main_ ":
+  app.run()
